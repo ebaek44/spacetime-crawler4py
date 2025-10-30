c2f20e4f6af955081c4ac98690158da813524477
caleb vergene <calebverg@gmail.com>
2025-10-30 01:02:05 -0700

    func to check if page too little info

--- scraper.py
+++ scraper.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 from validate_url_helpers import *
 from validate_html_helpers import *
 
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
+
 
 def extract_next_links(url, resp):
     if resp.status != 200:
@@ -30,6 +32,7 @@ def extract_next_links(url, resp):
         links.append(defragged)
 
     return links
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
--- validate_html_helpers.py
+++ validate_html_helpers.py
@@ -3,11 +3,27 @@ def page_low_content(soup):
     words = text.split()
     if len(words) < 100:
         return True
+    if page_little_information(soup, words): 
+        return True
     return False
 
 
 def page_too_large(resp):
     content = resp.raw_response.content
     if len(content) > 5_000_000:
+        return True
+    return False
+
+
+def page_little_information(soup, words):
+    # too many repetitive words
+    unique_words = set(words)
+    if len(unique_words) < 50:
+        return True
+    
+    # this compares text to html ratio
+    html = str(soup)
+    ratio = len(words) / len(html) if html_length > 0 else 0
+    if ratio < 0.1:
         return True
     return False
